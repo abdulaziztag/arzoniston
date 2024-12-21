ec550f156dc25ba91a8836c6ed3100ad6739b789
Abdulaziz <mannopov481@gmail.com>
2024-12-21 19:28:18 +0500

    filters added

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,19 @@
 import { getCarAdvertisements, getCarBrands, getPlaces } from '@/api';
 import CarAdvertisementList from '@/app/components/CarAdvertisementList';
 
-export default async function Home() {
-  const carAdvertisements = await getCarAdvertisements({});
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Record<string, string | undefined>;
+}) {
+  const carAdvertisements = await getCarAdvertisements({ params: searchParams });
   const carBrands = await getCarBrands();
   const carPlaces = await getPlaces();
 
   return (
     <CarAdvertisementList
       carPlaces={carPlaces}
-      carAdvertisements={carAdvertisements.results}
+      carAdvertisements={carAdvertisements}
       carBrands={carBrands}
     />
   );
